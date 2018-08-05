23603cbd2ad0f71aa4b9d2aee316d82826fe7338
Benjamin Geese <code@benjamingeese.de>
2018-08-05 16:07:32 +0200

    Add image resources

---
+++ app/app/src/main/java/de/benjamingeese/plantbuddy/SelectPlantTypeActivity.java
@@ -0,0 +1,13 @@
+package de.benjamingeese.plantbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectPlantTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_plant_type);
+    }
+}
---
+++ images/readme.txt
@@ -0,0 +1,4 @@
+Cactus Photo 
+Photo by Annie Spratt on Unsplash
+example-plant-image.png
+https://unsplash.com/photos/fbAnIjhrOL4
